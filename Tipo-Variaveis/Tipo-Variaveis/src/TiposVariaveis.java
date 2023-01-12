9cd68c57c710fa24e03c6fae93ffbb01eb2a1161
Leonardo <leo.ancora2@gmail.com>
2023-01-12 11:28:39 -0300

    tipos variaveis finalizado

--- Tipo-Variaveis/Tipo-Variaveis/src/TiposVariaveis.java
+++ Tipo-Variaveis/Tipo-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        //Estudar
+        //Tipos primitivos
+        //Estudem a classe String que representa texto na aplicação. 
+
+        String meuNome =  "Leonardo Fox"
+        
+         
+        double  salarioMinimo = 2500;
+    
+        short  numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short   numeroCurso2  = (short) numeroNormal;
+    
+        int numero = 5;
+    
+        numero = 10;
+    
+        System.out.print(numero);
+    
+        final double VALOR_DE_PI = 3.14;
+    
+        }
 }
